a1e441783e9ee8d39b02ae1407dec72100132b24
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-11-30 13:21:48 +0100

    Antibiotic tolerance

---
+++ 1.4/Source/VanillaRacesExpanded-Genie/VanillaRacesExpanded-Genie/DefOfs/InternalDefOf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedGenies
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+       
+
+
+    }
+}
