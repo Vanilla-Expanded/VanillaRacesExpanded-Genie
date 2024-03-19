04554b4cb3d619ce2e0b111769b01035a15fa936
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-19 16:52:25 +0100

    1.5 update

---
+++ 1.5/Source/VanillaRacesExpanded-Genie/VanillaRacesExpanded-Genie/IngestionOutcomeDoers/IngestionOutcomeDoer_GiveHediff_Discriminating.cs
@@ -0,0 +1,65 @@
+﻿
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+namespace VanillaRacesExpandedGenies
+{
+	public class IngestionOutcomeDoer_GiveHediff_Discriminating : IngestionOutcomeDoer
+	{
+		public HediffDef hediffDef;
+		public HediffDef firstAlternateHediffDef;
+		public HediffDef secondAlternateHediffDef;
+		public HediffDef thirdAlternateHediffDef;
+
+
+
+		public float severity = -1f;
+
+		public ChemicalDef toleranceChemical;
+
+		private bool divideByBodySize;
+
+		public bool multiplyByGeneToleranceFactors;
+
+		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestAmount)
+		{
+			Hediff hediff;
+
+			hediff = HediffMaker.MakeHediff(hediffDef, pawn);
+			if (pawn.genes?.HasGene(InternalDefOf.VRE_Immunity_VeryWeak) == true && pawn.genes?.GetGene(InternalDefOf.VRE_Immunity_VeryWeak).Active==true)
+			{
+				hediff = HediffMaker.MakeHediff(firstAlternateHediffDef, pawn);
+			}
+			if (DefDatabase<GeneDef>.GetNamedSilentFail("AG_Gene_VeryWeakImmunity") !=null &&pawn.genes?.HasGene(DefDatabase<GeneDef>.GetNamed("AG_Gene_VeryWeakImmunity")) == true 
+				&& pawn.genes?.GetGene(DefDatabase<GeneDef>.GetNamed("AG_Gene_VeryWeakImmunity")).Active == true)
+			{
+				hediff = HediffMaker.MakeHediff(secondAlternateHediffDef, pawn);
+			}
+			if (pawn.genes?.HasGene(InternalDefOf.Immunity_Weak) == true && pawn.genes?.GetGene(InternalDefOf.Immunity_Weak).Active == true)
+			{
+				hediff = HediffMaker.MakeHediff(thirdAlternateHediffDef, pawn);
+			}
+
+
+			float effect = (!(severity > 0f)) ? hediffDef.initialSeverity : severity;
+			if (divideByBodySize)
+			{
+				effect /= pawn.BodySize;
+			}
+			AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize(pawn, toleranceChemical, ref effect, multiplyByGeneToleranceFactors);
+			hediff.Severity = effect;
+			pawn.health.AddHediff(hediff);
+		}
+
+		public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+		{
+			if (parentDef.IsDrug && chance >= 1f)
+			{
+				foreach (StatDrawEntry item in hediffDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+				{
+					yield return item;
+				}
+			}
+		}
+	}
+}
